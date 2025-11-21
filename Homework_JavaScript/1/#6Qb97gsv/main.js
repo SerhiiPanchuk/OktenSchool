d93989bd100e88aa48c38d2700e498da759e9606
SerhiiPanchuk <forestninja777@gmail.com>
2025-11-21 21:50:52 +0200

    homework_javaScript

---
+++ Homework_JavaScript/1/#6Qb97gsv/main.js
@@ -0,0 +1,4 @@
+let firstName = "Сергій";
+let middleName = "Панчук";
+let lastName = "Валерійович";
+let person = `${firstName} ${middleName} ${lastName}`
---
+++ Homework_JavaScript/1/#ruUtWDUI/main.js
@@ -0,0 +1,5 @@
+let firstName = prompt("Вкажіть ваше ім'я");
+let lastName = prompt("Вкажіть як вас по-батькові");
+let age =  prompt("Вкажіть ваш вік");
+let person = `${firstName} ${lastName} ${age}`;
+console.log(person);
